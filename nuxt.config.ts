6fef5de09cb84e1aeaf7356d691a12101891de2f
Alois Sečkár <ellrohir@seznam.cz>
2024-03-30 18:35:05 +0100

    feat: switch to new nuxt/eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: [
     'nuxt-time',
     'nuxt-security',
+    '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui',
     '@nuxtjs/i18n',
@@ -11,6 +12,11 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@vueuse/nuxt'
   ],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
   i18n: {
     // if required to modify i18n
   },
@@ -18,16 +24,16 @@ export default defineNuxtConfig({
     // if required to modify nuxt/image
     presets: {
       // for custom presets
-    }
+    },
   },
   supabase: {
     // if required to modify supabase
-    redirect: false // https://github.com/supabase/supabase/issues/16551#issuecomment-1685300935
+    redirect: false, // https://github.com/supabase/supabase/issues/16551#issuecomment-1685300935
   },
   runtimeConfig: {
     // nitro-only secret env-like variables goes here
     public: {
       // client-exposed env-like variables goes here
-    }
-  }
+    },
+  },
 })
