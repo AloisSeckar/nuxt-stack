602d55e155f1db9bd324c82413fab80b62c45d7c
Alois Sečkár <ellrohir@seznam.cz>
2024-12-21 18:06:07 +0100

    fix: types definition for i18n

--- utils/i18n-sources.ts
+++ utils/i18n-sources.ts
@@ -1,14 +1,16 @@
 // scans @/assets/lang/* for all .json files with i18n messages
 // and makes them available for i18n.config.ts
 
+import type { LocaleMessage } from '@intlify/core-base'
+
 export function scanI18NSources() {
   const modules = import.meta.glob('@/assets/lang/*.json', { eager: true })
 
-  const messages = {} as Record<string, unknown>
+  const messages = {} as { [x: string]: LocaleMessage<unknown> }
   for (const path in modules) {
     const locale = path.match(/\/([^/]+)\.json$/)
     if (locale && locale[1]) {
-      messages[locale[1]] = modules[path]
+      messages[locale[1]] = modules[path] as LocaleMessage<unknown>
     }
   }
 
